64d2016810c8a51043960861379b2f4e35985794
bielpedr0 <gpedroalves@gmail.com>
2023-11-09 16:34:46 -0300

    Tela Calendario (teste)

---
+++ PE2/FrmCalendario.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PE2
+{
+    partial class FrmCalendario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCalendario));
+            this.panelLateralAberto = new System.Windows.Forms.Panel();
+            this.labelCalendar = new System.Windows.Forms.Label();
+            this.btnCalendar = new System.Windows.Forms.Button();
+            this.labelMenu = new System.Windows.Forms.Label();
+            this.labelDashboard = new System.Windows.Forms.Label();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.homeLabel = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.panelMenuGoldOption = new System.Windows.Forms.Panel();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panelLateralAberto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLateralAberto
+            // 
+            this.panelLateralAberto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(1)))));
+            this.panelLateralAberto.Controls.Add(this.labelCalendar);
+            this.panelLateralAberto.Controls.Add(this.btnCalendar);
+            this.panelLateralAberto.Controls.Add(this.labelMenu);
+            this.panelLateralAberto.Controls.Add(this.labelDashboard);
+            this.panelLateralAberto.Controls.Add(this.btnDashboard);
+            this.panelLateralAberto.Controls.Add(this.homeLabel);
+            this.panelLateralAberto.Controls.Add(this.btnHome);
+            this.panelLateralAberto.Controls.Add(this.btnMenu);
+            this.panelLateralAberto.Controls.Add(this.panelMenuGoldOption);
+            this.panelLateralAberto.Location = new System.Drawing.Point(2, 0);
+            this.panelLateralAberto.Margin = new System.Windows.Forms.Padding(4);
+            this.panelLateralAberto.Name = "panelLateralAberto";
+            this.panelLateralAberto.Size = new System.Drawing.Size(155, 690);
+            this.panelLateralAberto.TabIndex = 2;
+            // 
+            // labelCalendar
+            // 
+            this.labelCalendar.AutoSize = true;
+            this.labelCalendar.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelCalendar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelCalendar.Location = new System.Drawing.Point(75, 332);
+            this.labelCalendar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCalendar.Name = "labelCalendar";
+            this.labelCalendar.Size = new System.Drawing.Size(62, 16);
+            this.labelCalendar.TabIndex = 8;
+            this.labelCalendar.Text = "Calendar";
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCalendar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalendar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalendar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCalendar.Image = ((System.Drawing.Image)(resources.GetObject("btnCalendar.Image")));
+            this.btnCalendar.Location = new System.Drawing.Point(9, 315);
+            this.btnCalendar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(48, 50);
+            this.btnCalendar.TabIndex = 7;
+            this.btnCalendar.UseMnemonic = false;
+            this.btnCalendar.UseVisualStyleBackColor = false;
+            // 
+            // labelMenu
+            // 
+            this.labelMenu.AutoSize = true;
+            this.labelMenu.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelMenu.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelMenu.Location = new System.Drawing.Point(75, 30);
+            this.labelMenu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMenu.Name = "labelMenu";
+            this.labelMenu.Size = new System.Drawing.Size(40, 16);
+            this.labelMenu.TabIndex = 5;
+            this.labelMenu.Text = "Menu";
+            // 
+            // labelDashboard
+            // 
+            this.labelDashboard.AutoSize = true;
+            this.labelDashboard.BackColor = System.Drawing.Color.Transparent;
+            this.labelDashboard.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelDashboard.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelDashboard.Location = new System.Drawing.Point(73, 222);
+            this.labelDashboard.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDashboard.Name = "labelDashboard";
+            this.labelDashboard.Size = new System.Drawing.Size(75, 16);
+            this.labelDashboard.TabIndex = 4;
+            this.labelDashboard.Text = "Dashboard";
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.Transparent;
+            this.btnDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDashboard.ForeColor = System.Drawing.Color.Transparent;
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.Location = new System.Drawing.Point(9, 204);
+            this.btnDashboard.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(48, 50);
+            this.btnDashboard.TabIndex = 3;
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // homeLabel
+            // 
+            this.homeLabel.AutoSize = true;
+            this.homeLabel.ForeColor = System.Drawing.Color.Cornsilk;
+            this.homeLabel.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.homeLabel.Location = new System.Drawing.Point(75, 121);
+            this.homeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.homeLabel.Name = "homeLabel";
+            this.homeLabel.Size = new System.Drawing.Size(44, 16);
+            this.homeLabel.TabIndex = 2;
+            this.homeLabel.Text = "Home";
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Transparent;
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHome.ForeColor = System.Drawing.Color.Transparent;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.Location = new System.Drawing.Point(9, 103);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(4);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(48, 50);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.UseMnemonic = false;
+            this.btnHome.UseVisualStyleBackColor = false;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMenu.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMenu.Image = ((System.Drawing.Image)(resources.GetObject("btnMenu.Image")));
+            this.btnMenu.Location = new System.Drawing.Point(9, 12);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(48, 50);
+            this.btnMenu.TabIndex = 0;
+            this.btnMenu.UseVisualStyleBackColor = true;
+            // 
+            // panelMenuGoldOption
+            // 
+            this.panelMenuGoldOption.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(216)))), ((int)(((byte)(22)))));
+            this.panelMenuGoldOption.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelMenuGoldOption.Location = new System.Drawing.Point(0, 299);
+            this.panelMenuGoldOption.Margin = new System.Windows.Forms.Padding(67, 62, 67, 62);
+            this.panelMenuGoldOption.Name = "panelMenuGoldOption";
+            this.panelMenuGoldOption.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.panelMenuGoldOption.Size = new System.Drawing.Size(155, 84);
+            this.panelMenuGoldOption.TabIndex = 6;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(476, 165);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(476, 131);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // FrmCalendario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 690);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.panelLateralAberto);
+            this.Name = "FrmCalendario";
+            this.Text = "FrmCalendario";
+            this.panelLateralAberto.ResumeLayout(false);
+            this.panelLateralAberto.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLateralAberto;
+        private System.Windows.Forms.Label labelCalendar;
+        private System.Windows.Forms.Button btnCalendar;
+        private System.Windows.Forms.Label labelMenu;
+        public System.Windows.Forms.Label labelDashboard;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Label homeLabel;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.Panel panelMenuGoldOption;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
